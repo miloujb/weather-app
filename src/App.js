d66e1c4a3a4786ab3037e1b41c2ad56185303bb5
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-23 17:44:00 +0100

    weather component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Form from "./Components/Form/Form";
 import Titles from "./Components/Titles/Titles";
+import Weather from "./Components/Weather/Weather";
 
 function App() {
   return (
     <div className="App">
       <Titles />
       <Form />
+      <Weather />
     </div>
   );
 }
---
+++ src/Components/Weather/Weather.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Weather extends Component {
+  render() {
+    return (
+      <div>
+        <p>The Weather</p>
+      </div>
+    );
+  }
+}
+
+export default Weather;
