a3a6720f76aca820de63a56dd6b70df85188e412
miloujb <emilyjbailey@hotmail.co.uk>
2019-10-23 18:42:34 +0100

    api set up, not fetching due to cors error

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import Form from "./Components/Form/Form";
 import Titles from "./Components/Titles/Titles";
 import Weather from "./Components/Weather/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <Titles />
-      <Form />
-      <Weather />
-    </div>
-  );
+const API_key = "a02be84938b6870f46fba5c56f9c1026";
+
+class App extends React.Component {
+  getWeather = async e => {
+    e.preventDefault();
+    const callAPI = await fetch(
+      `https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}&units=metric`
+    );
+    const data = await callAPI.json();
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
